835990c05eb6e93c053570d5a4e9357f67200a8d
nabilamudikaputri <nabilamudikaputri@gmail.com>
2026-04-28 23:32:42 +0700

    refactor: move CRUD logic from controller to service layer

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -6,39 +6,43 @@ use App\Services\MovieService;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Http\Requests\StoreMovieRequest;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class MovieController extends Controller
 {
-
-    protected $movieService;
+    /**
+     * @var MovieService
+     */
+    protected MovieService $movieService;
 
     public function __construct(MovieService $movieService)
     {
         $this->movieService = $movieService;
     }
 
-        public function index()
+    public function index(): View
     {
         $movies = $this->movieService->getAll(request('search'));
         return view('homepage', compact('movies'));
     }
 
-    //  DETAIL
-    public function detail($id)
+    // DETAIL
+    public function detail(int $id): View
     {
         $movie = $this->movieService->getById($id);
         return view('detail', compact('movie'));
     }
 
-    //  FORM CREATE
-    public function create()
+    // FORM CREATE
+    public function create(): View
     {
         $categories = Category::all();
         return view('input', compact('categories'));
     }
 
-    //  STORE
-    public function store(StoreMovieRequest $request)
+    // STORE
+    public function store(StoreMovieRequest $request): RedirectResponse
     {
         $this->movieService->store(
             $request->validated(),
@@ -48,15 +52,15 @@ class MovieController extends Controller
         return redirect('/')->with('success', 'Film berhasil ditambahkan.');
     }
 
-    //  DATA MOVIES
-    public function data()
+    // DATA MOVIES
+    public function data(): View
     {
         $movies = $this->movieService->getAll();
         return view('data-movies', compact('movies'));
     }
 
-    //  FORM EDIT
-    public function form_edit($id)
+    // FORM EDIT
+    public function form_edit(int $id): View
     {
         $movie = $this->movieService->getById($id);
         $categories = Category::all();
@@ -64,8 +68,8 @@ class MovieController extends Controller
         return view('form-edit', compact('movie', 'categories'));
     }
 
-    //  UPDATE
-    public function update(Request $request, $id)
+    // UPDATE
+    public function update(Request $request, int $id): RedirectResponse
     {
         $this->movieService->update(
             $id,
@@ -77,7 +81,7 @@ class MovieController extends Controller
     }
 
     // DELETE
-    public function delete($id)
+    public function delete(int $id): RedirectResponse
     {
         $this->movieService->delete($id);
 
