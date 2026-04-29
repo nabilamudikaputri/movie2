d48a476fa1e110b38fc15d7dc4796c74efdb6053
nabilamudikaputri <nabilamudikaputri@gmail.com>
2026-04-29 23:13:00 +0700

    ganti teks

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -38,7 +38,7 @@
       </div>
 
       <footer class="bg-success text-center text-white py-2">
-        Copyright &copy; 2023 by Nabila Mudika Putri
+        Copyright &copy; 2023 by Yori Adi Adma
       </footer>
 
     <script src="/bootstrap/bootstrap.bundle.min.js"></script>
